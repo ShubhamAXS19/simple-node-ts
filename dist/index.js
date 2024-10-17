54d158db87f029d29c6db0e78ac53eca755cf46f
Shubham Vishwakarma <sv09734@gmail.com>
2024-10-17 21:22:39 +0530

    init commmit

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World");
 });
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });

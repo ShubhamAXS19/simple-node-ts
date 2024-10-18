1d89fe7ec7a2b146a76e79b860b71536069b1229
Shubham Vishwakarma <sv09734@gmail.com>
2024-10-18 11:13:16 +0530

    testing 4

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+app.get("/mongo", (req, res) => {
+  res.send(process.env.MONGO_URI);
 });
 
 app.listen(process.env.PORT, () => {
